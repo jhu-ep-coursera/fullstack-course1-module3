de0434ceca4678b09d87f8451d0f61394d054241
Kalman Hazins <kalmanh@gmail.com>
2015-08-13 17:15:34 -0400

    Before lecture 6

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,7 +1,10 @@
 class GreeterController < ApplicationController
   def hello
-  end
-  def goodbye
+  	random_names = ["Alex", "Joe", "Michael"]
+  	@name = random_names.sample
+  	@time = Time.now
+  	@times_displayed ||= 0
+  	@times_displayed += 1
   end
 end
 
